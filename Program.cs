d70dda186cba2786a1de7236787bdb3f02d57158
halyfergabriel5-maker <halyfergabriel5@gmail.com>
2026-09-05 15:48:30 -0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿//Faça um programa que exiba o primeiro caractere digitado, ignorando espaços em branco.
-string palavra = Console.ReadLine();
-Console.WriteLine(palavra[0]);
-/* primeiro criei uma variavel com o nome de "palavra", 
-coloquei essa variavel para capturar a palavra escrita pelo usuario usando Console.Readline
-depois disso usei console.writeline para exibir a variavel, colocando um indice[0] junto da variavel
-para retornar o char[0] ou primeiro caracter da palavra.*/
